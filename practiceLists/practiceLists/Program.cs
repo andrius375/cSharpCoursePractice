c95d3e1ddd8bb23adb67cdd11600309541577965
andrius375 <andrius_ivanauskas@yahoo.com>
2018-04-25 15:40:12 +0300

    Sarasai, padariau kiek atsiminiau uzduociu be skaidriu.

--- practiceLists/practiceLists/Program.cs
+++ practiceLists/practiceLists/Program.cs
@@ -1,348 +1,89 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-class Program
+namespace List
 {
-
-    static void Main()
+    class Program
     {
-        // TODO : keiskite FROM..TO skaicius pagal tai kiek spesite padaryt uzduociu. (-19...19, -99..99, ir tt.)
-        // min skaicius 
-        const int FROM_NUMBER = -999;
-        // max skaicius 
-        const int TO_NUMBER = 999;
-
-        string inputString = "";
-        int inputNumber = 0;
-
-        Console.Write("Sveiki!");
-        while (inputString != " ")
+        static void Main(string[] args)
         {
-            Console.Write("\n(Enter SPACE to exit.)\nIveskite skaiciu:");
-            inputString = Console.ReadLine();
-            if (checkIfGoodNumber(inputString))
+            string bigStringToList = @"Žinote, tą jausmą, kai savaitgalį norisi pamiegoti ilgiau, bet kaimynai anksti pradeda remontuoti savo namus? Gręžimas, kalimas, keiksmai ir labai triukšmingi įrankiai - viso to pakanka sugadinti jūsų dienai. Tačiau visa tai - niekis, palyginti su didžiausiu žmogaus sukurtu garsu.
+Motociklo garsas vos už 8 metrų siekia tik 90 dB 
+©Piero | commons.wikimedia.org
+Žmonės yra labai triukšmingi padarai. Kartais šalia judrios gatvės sunku ir susikalbėti, o didžiųjų miestų gyventojai labai retai gali patirti visišką tylą. Akustinė tarša, beje, labai kenkia sveikatai. Žmogui reikia poilsio, o triukšme jis apskritai negalimas. Net jei jūs išmokstate triukšmą ignoruoti, jis vis tiek jus veikia ir trumpina jūsų gyvenimą. Tačiau miesto triukšmas, palyginti su garsiausiais mūsų sukeltais garsais, nėra toks jau blogas.
+Šnabždesys sukelia maždaug 20 decibelų garsą - jo išgirsti per keletą metrų jau neįmanoma. Daugelis paukščių gieda maždaug 44 dB garsu - vėlgi, jūsų langai tą triukšmą blokuoja. Kita vertus, mažai kas skundžiasi paukščių giesmėmis. Įprastas pokalbis namuose sukelia 50 dB garsą, o geras dulkių siurblys - 70. Maždaug čia prasideda garsai, kurių mes negalime pakęsti ilgesnį laiką. Štai gero motociklo garsas vos už 8 metrų siekia net 90 decibelų. Tokį garsą sukėlė ir Čeliabinsko meteoritas už 650 km. Tačiau tokie garsai nekenkia jau vien todėl, kad tokiame triukšme žmogus ilgai nebūna.
+Tuo tarpu tokiais įrankiais kaip pneumatinis kūjis tenka pasidarbuoti ilgiau - jis sukelia 100-120 dB garsą, priklausomai nuo to, su kokiomis medžiagomis dirbama. Lėktuvnešio denyje dažnai pasigirsta 140 dB garsas, todėl visi ten dirbantys žmonės dėvi garsą blokuojančias ausines. NASA akustinių tyrimų kameroje palydovai išbandomi net 163 dB garsu, kurį žmonėms ilgą laiką ištverti būtų jau labai sunku. Tačiau šalia „Saturn V“ raketos paleidimo aikštelės galima užfiksuoti net 204 dB garsą, kuris gali sukelti negrįžtamą žalą sveikatai.
+Povandeninio laivo sonaras skleidžia nepaprastai didelį garsą 
+©Paul Halliwell/MOD | commons.wikimedia.org
+Taigi, raketos yra nepaprastai triukšmingos, bet tokios jos neturi būti. Ateityje galbūt bus išrasti kitokie raketiniai varikliai, kurie tokio didžiulio garso nesukels. Kitaip tariant, tas milžiniškas garsas nėra būtinas raketos veikimui. Tačiau didžiausias žmogaus sukeltas garsas iš tiesų yra būtinas. Tai - povandeninių laivų sonarai.
+Garsas vandenyje sklinda visiškai kitaip nei ore. Bangos vandenyje yra daug stipresnės, todėl jas nesunku panaudoti savo reikmėms. Banginiai naudoja echolokaciją. Jie skleidžia garsus ir jaučia kaip bangos atsimuša į įvairius objektus ir sugrįžta. Banginių skleidžiamas garsas yra apie 175 dB, tačiau jie neprilygsta povandeninių laivų sonarams, kurie skleidžia didesnius nei 230 dB signalus.
+Šis garsas yra toks stiprus, kad nardytojai lekia iš vandens, o banginiai užplaukia ant sausumos. Iš tiesų, povandeninių laivų sonarai veikia panašiai, kaip ir banginių echolokacijos sistema - sonarai stebi sugrįžtantį aidą ir pagal jį nustato įvairių objektų formas ir atstumą. Sonaro veikimą gali išgirsti kiti povandeniniai laivai, todėl tenka pasitelkti strategiją ir savo sonarą naudoti tik tada, kai tai daryti saugu.
+Jei žmonės norėtų panašų garsą atkurti sausumoje, tikriausiai reikėtų pasitelkti visą eilę sprogimų. O štai gamta tai gali padaryti ugnikalnio išsiveržimu. Manoma, kad 1883 metų Karakatau išsiveržimas sukėlė kur kas didesnį garsą nei povandeninio laivo sonaras, nes net už 160 km garso stiprumas siekė 172 dB.";
+
+            List<string> text = bigStringToList.Split(' ').ToList();
+
+            Console.WriteLine("1. Parodyti originalų tekstą;");
+            Console.WriteLine("2. Pašalinti žodį iš teksto;");
+            Console.WriteLine("3. Kiek elementų turi tekstas?");
+            Console.WriteLine("4. Kiek pasikartojančių žodžių turi tekstas?");
+            // Console.ReadKey();
+
+            int chosenMenu = int.Parse(Console.ReadLine());
+
+            switch (chosenMenu)
             {
-                Console.WriteLine("Skaicius teisingas!");
-                inputNumber = Convert.ToInt32(inputString);
-                if (checkIfNumberInRange(FROM_NUMBER, TO_NUMBER, inputNumber))
-                {
-                    int firstDigit = inputNumber / 10; //papildomi kintamieji suskaldyt skaiciu >20 i du skaitmenis, pagal kuriuos nustatysim kokiam pozicija
-                    int lastDigits = inputNumber % 10; //papildomi kintamieji suskaldyt skaiciu >20 i du skaitmenis, pagal kuriuos nustatysim kokiam pozicija
+                case 1:
+                    printList(text);
+                    break;
+                case 2:
+                    Console.Write("Koki zodi pasalint?");
+                    string wordToRemoveFromList = Convert.ToString(Console.ReadLine());
+                    text.Remove(wordToRemoveFromList);
+                    printList(text);
+                    break;
+                case 3:
+                    int wordCount = text.Count();
+                    Console.WriteLine("Tekstas turi " + wordCount + " elementu.");
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    Console.WriteLine("Tekstas turi " + countWords(text) + " žodžių.");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Netinkamas pasirinkimas!");
+                    Console.ReadKey();
+                    break;
+            }
 
-                    if (inputNumber >= 0 && inputNumber <= 10) // nuo 0 iki 10
-                    {
-                        Console.WriteLine("Skaicius {0} zodziais: {1}", inputNumber, changeNumberToText(inputNumber));
-                    }
-                    else if (inputNumber < 0 && inputNumber >= -10)// nuo -1 iki -10
-                    {
-                        Console.WriteLine("Skaicius {0} zodziais: minus {1}", inputNumber, changeNumberToText(inputNumber));
-                    }
-                    else if (inputNumber < -10 && inputNumber > -20)// nuo -11 iki -19
-                    {
-                        Console.WriteLine("Skaicius {0} zodziais: minus {1}", inputNumber, changeNumberToText(inputNumber));
-                    }
-                    else if (inputNumber >= 20 && inputNumber < 100) // nuo 20 iki 99
-                    {
-                        if (lastDigits != 0) // papildoma apsauga kad ivedus pvz: 20 - nerasytu atsakymo - dvidesimt nulis
-                        {
-                            Console.WriteLine("Skaicius {0} zodziais: {1}", inputNumber, changeTensToText(firstDigit) + changeNumberToText(lastDigits));
-                        }
-                        else
-                        {
-                            Console.WriteLine("Skaicius {0} zodziais: {1}", inputNumber, changeTensToText(firstDigit));
-                        }
-                    }
-                    else if (inputNumber < -19 && inputNumber > -101) // nuo -20 iki -99
-                    {
-                        if (lastDigits != 0)// papildoma apsauga kad ivedus pvz: -20 - nerasytu atsakymo - minus dvidesimt nulis
-                        {
-                            Console.WriteLine("Skaicius {0} zodziais: minus {1}", inputNumber, changeTensToText(firstDigit) + changeNumberToText(lastDigits));
-                        }
-                        else
-                        {
-                            Console.WriteLine("Skaicius {0} zodziais: minus {1}", inputNumber, changeTensToText(firstDigit));
-                        }
-                    }
+        }
 
-                    else // atspausdina nuo 11 iki 19
-                    {
-                        Console.WriteLine("Skaicius {0} zodziais: {1}", inputNumber, changeNumberToText(inputNumber));
-                    }
-                }
-                else
+        static void printList(List<string> text)
+        {
+            foreach (string tekst in text)
+
+                Console.Write(" " + tekst);
+            Console.WriteLine("\n============================================");
+
+            Console.ReadKey();
+        }
+        static string countWords(List<string> text)
+        {
+            int dublicatesWrodsInText = 0;
+            for (int i = 0; i < text.Count; i++)
+            {
+                for (int j = i + 1; j < text.Count; j++)
                 {
-                    Console.WriteLine("Blogas skaicius {0}, prasau ivesti skaiciu reziuose: {1}..{2}", inputString, FROM_NUMBER, TO_NUMBER);
+                    if (text[i] == text[j] && i != j)
+                    {
+                        dublicatesWrodsInText++;
+                    }
                 }
             }
-            else
-            {
-                Console.WriteLine("Ivesti duomenys:{0} nera skaicius!", inputString);
-            }
+            return dublicatesWrodsInText.ToString();
         }
-        Console.WriteLine("\nAciu uz demesi, viso gero.");
-        Console.ReadKey();
-    }
-
-    // bendra funkcija apjungti visom funkcijom kurias jus sukursit.
-    static string changeNumberToText(int number)
-    {
-        string changing = "";//sukuriau tuscia string, ant kurio bus rasomi rezultatai zodziu atlikus salygas ir grazinamas i funkcija virsuje 
-        // TODO : pakeiskite sita funkcija pagal savo poreiki. (tiek kiek skaiciu spesite apdorot.)
-        if (number > -11 && number < 11)
-        {
-            changing = changeOnesToText(number);
-        }
-        else if (number < -10 && number > -20 || number > 10 && number < 20)
-        {
-            changing = changeTeensToText(number);
-        }
-        else if (number < -19 && number > -100 || number > 19 && number < 100)
-        {
-            changing = changeTensToText(number);
-        }
-        return changing;
-    }
-    // funkcija gauna string skaiciu, patikrina ar skaicius teisingu formatu. Pvz: "123", "-123" grazina true. "12a3", "1-23" grazina false.
-    static bool checkIfGoodNumber(string dataToCheck)
-    {
-        //tikrinimas3 tiesiog bool kintamasis
-        bool tikrinimas3 = false;
-        for (int i3 = 0; i3 < dataToCheck.Length; i3++)//sukurtas ciklas praeis pro visus irasytus simbolius iki kol pasieks zodzio gala
-        {
-            if (i3 == 0)
-            {
-                char hm3 = dataToCheck[i3];//susikuriau atskirai char ir prilyginau gautiems simboliams
-                if (hm3 != 48 && hm3 != 49 && hm3 != 50 && hm3 != 51 && hm3 != 52 && hm3 != 53 && hm3 != 54 && hm3 != 55 && hm3 != 56 && hm3 != 57 && hm3 != 58)
-                {     //salyga tikrina simbolius nuo 0 iki 9
-                    if (hm3 == 45) // patikrina ar prieki yra minusas
-                    {
-                        tikrinimas3 = true;
-                    }
-                    else
-                    {
-                        tikrinimas3 = false;
-                    }
-                }
-                else
-                {
-                    tikrinimas3 = true;
-                }
-            }
-            if (i3 == 1)
-            {
-                char hm3 = dataToCheck[i3];
-                if (hm3 != 48 && hm3 != 49 && hm3 != 50 && hm3 != 51 && hm3 != 52 && hm3 != 53 && hm3 != 54 && hm3 != 55 && hm3 != 56 && hm3 != 57 && hm3 != 58)
-                {
-                    tikrinimas3 = false;
-                }
-                else
-                {
-                    tikrinimas3 = true;
-                }
-            }
-            if (i3 == 2)
-            {
-                char hm3 = dataToCheck[i3];
-                if (hm3 != 48 && hm3 != 49 && hm3 != 50 && hm3 != 51 && hm3 != 52 && hm3 != 53 && hm3 != 54 && hm3 != 55 && hm3 != 56 && hm3 != 57 && hm3 != 58)
-                {
-                    tikrinimas3 = false;
-                }
-                else
-                {
-                    tikrinimas3 = true;
-                }
-            }
-            if (i3 == 3)
-            {
-                char hm3 = dataToCheck[i3];
-                if (hm3 != 48 && hm3 != 49 && hm3 != 50 && hm3 != 51 && hm3 != 52 && hm3 != 53 && hm3 != 54 && hm3 != 55 && hm3 != 56 && hm3 != 57 && hm3 != 58)
-                {
-                    tikrinimas3 = false;
-                }
-                else
-                {
-                    tikrinimas3 = true;
-                }
-            }
-            if (i3 == 4)
-            {
-                char hm3 = dataToCheck[i3];
-                if (hm3 != 48 && hm3 != 49 && hm3 != 50 && hm3 != 51 && hm3 != 52 && hm3 != 53 && hm3 != 54 && hm3 != 55 && hm3 != 56 && hm3 != 57 && hm3 != 58)
-                {
-                    tikrinimas3 = false;
-                }
-                else
-                {
-                    tikrinimas3 = true;
-                }
-            }
-        }
-        return tikrinimas3;
-    }
-    // funkcija gauna true jei skaicius checkNumber yar tarp fromNumber ir toNumber (imtinai)
-    private static bool checkIfNumberInRange(int fromNumber, int toNumber, int checkNumber)
-    {
-        bool tikrinimas = false;
-        if (checkNumber >= fromNumber && checkNumber <= toNumber)
-        {
-            tikrinimas = true;
-        }
-        else
-        {
-            tikrinimas = false;
-        }
-        return tikrinimas;
 
     }
-    // funkcija gauna int skaiciu, pakeicia ji i string teksta kuri zodziais nusako skaiciu. PVZ: -1684542 turi grazint - "minus vienas milijonas sesi simtai astuoniasdesimt keturi tukstanciai penki simtai keturiasdiasimt du"
-    static string changeOnesToText(int number)
-    {
-        string changeOnesToText = "";
-        if (number == -1 || number == 1)
-        {
-            changeOnesToText = "vienas";
-        }
-        if (number == -2 || number == 2)
-        {
-            changeOnesToText = "du";
-        }
-        if (number == -3 || number == 3)
-        {
-            changeOnesToText = "trys";
-        }
-        if (number == -4 || number == 4)
-        {
-            changeOnesToText = "keturi";
-        }
-        if (number == -5 || number == 5)
-        {
-            changeOnesToText = "penki";
-        }
-        if (number == -6 || number == 6)
-        {
-            changeOnesToText = "sesi";
-        }
-        if (number == -7 || number == 7)
-        {
-            changeOnesToText = "septyni";
-        }
-        if (number == -8 || number == 8)
-        {
-            changeOnesToText = "astuoni";
-        }
-        if (number == -9 || number == 9)
-        {
-            changeOnesToText = "devyni";
-        }
-        if (number == -10 || number == 10)
-        {
-            changeOnesToText = "desimt";
-        }
-        if (number == 0)
-        {
-            changeOnesToText = "nulis";
-        }
-        return changeOnesToText;
-    }
-
-    static string changeTeensToText(int number)
-    {
-        string changeTeensToText = "";
-        if (number == -11 || number == 11)
-        {
-            changeTeensToText = "vienuolika";
-        }
-        if (number == -12 || number == 12)
-        {
-            changeTeensToText = "dvylika";
-        }
-        if (number == -13 || number == 13)
-        {
-            changeTeensToText = "trylika";
-        }
-        if (number == -14 || number == 14)
-        {
-            changeTeensToText = "keturiolika";
-        }
-        if (number == -15 || number == 15)
-        {
-            changeTeensToText = "penkiolika";
-        }
-        if (number == -16 || number == 16)
-        {
-            changeTeensToText = "sesiolika";
-        }
-        if (number == -17 || number == 17)
-        {
-            changeTeensToText = "septyniolika";
-        }
-        if (number == -18 || number == 18)
-        {
-            changeTeensToText = "astuoniolika";
-        }
-        if (number == -19 || number == 19)
-        {
-            changeTeensToText = "devyniolika";
-        }
-        return changeTeensToText;
-    }
-
-    static string changeTensToText(int number)
-    {
-        string changeTensToText = "";
-        if (number == -2 || number == 2)
-        {
-            changeTensToText = "dvidesimt ";
-        }
-        if (number == -3 || number == 3)
-        {
-            changeTensToText = "trisdesimt ";
-        }
-        if (number == -4 || number == 4)
-        {
-            changeTensToText = "keturiasdesimt ";
-        }
-        if (number == -5 || number == 5)
-        {
-            changeTensToText = "penkiasdesimt ";
-        }
-        if (number == -6 || number == 6)
-        {
-            changeTensToText = "sesiasdesimt ";
-        }
-        if (number == -7 || number == 7)
-        {
-            changeTensToText = "septyniasdesimt ";
-        }
-        if (number == -8 || number == 8)
-        {
-            changeTensToText = "astuoniasdesimt ";
-        }
-        if (number == -9 || number == 9)
-        {
-            changeTensToText = "devyniasdesimt ";
-        }
-
-        return changeTensToText;
-    }
-
-
-
-    // TODO : sukurti funkcija kuri grazina skaiciu -999...999 zodziais - changeHundredsToText
-
-    // TODO : sukurti funkcija kuri grazina skaiciu -9999...9999 zodziais - changeThousandsToText
-
-    // TODO : sukurti funkcija kuri grazina skaiciu -9999999...9999999 zodziais - changeMillionsToText
-
-    // TODO : sukurti funkcija kuri grazina skaiciu -9999999999...9999999999 zodziais - changeBilllionsToText
-
-
-
-    //Skaiciai zodziais:  
-    // "minus"; 
-    // "nulis", "vienas", "du", "trys", "keturi", "penki", "sesi", "septyni", "astuoni", "devyni"; 
-    // "desimt", "vienualika", "dvylika", "trylika", "keturiolika", "penkiolika", "sesiolika", "septyniolika", "astuoniolika", "devyniolika"; 
-    // "dvidesimt", "trisdesimt", "keturiasdesimt", "penkiasdesimt", "sesiasdesimt", "septyniasdesimt", "astuoniasdesimt", "devyniasdesimt"; 
-    // "simtas", "tukstantis", "milijonas", "milijardas"; 
-    // "simtai", "tukstanciai", "milijonai", "milijardai"; 
-    // "simtu", "tukstanciu", "milijonu", "milijardu"; 
 }
