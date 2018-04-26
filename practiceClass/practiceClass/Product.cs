99efebc0112be75dc80f2c6dc287937237416140
andrius375 <andrius_ivanauskas@yahoo.com>
2018-04-27 00:11:55 +0300

    Sukurta klase "Product"

---
+++ practiceClass/practiceClass/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practiceClass
+{
+    class Product
+    {
+        private string Name;
+        private string Description;
+        private string Color;
+        private float Price;
+
+        public Product(string name, string description, string color, float price)
+        {
+            Name = name;
+            Description = description;
+            Color = color;
+            Price = price;
+        }
+        public string OnlyProductName()
+        {
+            return Name;
+        }
+      
+        public void Print()
+        {
+            Console.WriteLine("Prekes pavadinimas: {0}, aprasymas {1}, spalva {2}, kaina {3} EUR. ", Name, Description, Color, Price);
+        }
+    }
+}
---
+++ practiceClass/practiceClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practiceClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uzduotis: Sukurti klase: Product. Parduotuves prekei apibudinti.
+            Product phone = new Product("IPhone", "IPhone X 64GB", "black", 1178.9f);
+
+            phone.Print();
+            
+            Console.WriteLine(phone.OnlyProductName());
+            Console.ReadKey();
+        }
+    }
+}
